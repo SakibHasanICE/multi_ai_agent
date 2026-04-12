39d88596def39d59551707c2a75f0a799a9fc21d
SakibHasanICE <hasansakib047@gmail.com>
2026-04-12 06:12:12 +0600

    api.py backend fastappi

--- app/components/ai_agent.py
+++ app/components/ai_agent.py
@@ -2,7 +2,7 @@ from langchain_groq import ChatGrpq
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_prebuilt import create_react_agent
 from langchain_core.message.ai import AIMessage
-from app.config.settings import Settings
+from app.config.settings import settings
 
 def get_response_from_ai_agents(llm_id, query,allow_search,system_prompt):
     llm=ChatGrpq(model=llm_id)
